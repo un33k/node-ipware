71dd9517d28a77a926c9a3417a21dd5f94db0590
Val Neekman <val@neekware.com>
2014-10-08 00:04:23 -0400

    test clean up

--- test/ipv6_test.js
+++ test/ipv6_test.js
@@ -1,20 +1,16 @@
 var get_ip = require('..')().get_ip,
-    assert = require('assert');
+    assert = require('assert'),
+    request = {'headers': {'connection': {'remoteAddress': '172.0.0.1'}}};
 
 
 describe('HTTP_X_FORWARDED_FOR Multiple IPs', function() {
   it('should return best matched proxy IP address first', function() {
-    var request = {'headers':''};
-    request.headers = {
-      'HTTP_X_FORWARDED_FOR': '192.168.255.182, 10.0.0.0, 127.0.0.1, 198.84.193.157, 177.139.233.139',
-      'HTTP_X_REAL_IP': '177.139.233.132',
-      'REMOTE_ADDR': '177.139.233.133',
-      'connection': {
-        'remoteAddress': '172.0.0.1'
-      }
-    }
+    request.headers.HTTP_X_FORWARDED_FOR = '192.168.255.182, 10.0.0.0, 127.0.0.1, 198.84.193.157, 177.139.233.139';
+    request.headers.HTTP_X_REAL_IP = '177.139.233.132';
+    request.headers.REMOTE_ADDR = '177.139.233.133';
     get_ip(request);
     assert.equal(request.clientIp, '198.84.193.157');
+    assert.equal(request.clientIpRoutable, true);
   });
 });
 
