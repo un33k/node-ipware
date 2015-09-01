b2f14f952eb4316676a4b6b2aadd394a504eddff
Daniel Murley <danielmurley@daniels-mbp-2.osb.ft.com>
2015-09-01 19:37:31 +0100

    Added tests for X_FORWARDED_FOR. Changed test names to differentiate between X_FORWARDED_FOR and HTTP_X_FORWARDED_FOR.  Changed order of X_FORWARDED_FOR in defaults.json

--- test/ipv4_test.js
+++ test/ipv4_test.js
@@ -3,7 +3,7 @@ var get_ip = require('..')().get_ip,
 
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
-  it('test_x_forwarded_for_multiple', function() {
+  it('test_http_x_forwarded_for_multiple', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '192.168.255.182, 10.0.0.0, 127.0.0.1, 198.84.193.157, 177.139.233.139';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -15,7 +15,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
-  it('test_x_forwarded_for_multiple_right_most_proxy', function() {
+  it('test_http_x_forwarded_for_multiple_right_most_proxy', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '192.168.255.182, 198.84.193.157, 10.0.0.0, 127.0.0.1, 177.139.233.139';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -27,7 +27,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
-  it('test_x_forwarded_for_multiple_bad_address', function() {
+  it('test_http_x_forwarded_for_multiple_bad_address', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = 'unknown, 192.168.255.182, 10.0.0.0, 127.0.0.1, 198.84.193.157, 177.139.233.139';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -39,7 +39,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
-  it('test_x_forwarded_for_singleton', function() {
+  it('test_http_x_forwarded_for_singleton', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '177.139.233.139';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -51,7 +51,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
-  it('test_x_forwarded_for_singleton_private_address', function() {
+  it('test_http_x_forwarded_for_singleton_private_address', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '192.168.255.182';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -63,7 +63,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
-  it('test_bad_x_forwarded_for_fallback_on_x_real_ip', function() {
+  it('test_bad_http_x_forwarded_for_fallback_on_x_real_ip', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = 'unknown 177.139.233.139';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -75,7 +75,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
-  it('test_empty_x_forwarded_for_fallback_on_x_real_ip', function() {
+  it('test_empty_http_x_forwarded_for_fallback_on_x_real_ip', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '';
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
@@ -87,7 +87,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP', function() {
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', function() {
-  it('test_empty_x_forwarded_for_empty_x_real_ip_fallback_on_remote_addr', function() {
+  it('test_empty_http_x_forwarded_for_empty_x_real_ip_fallback_on_remote_addr', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '';
     request.headers.HTTP_X_REAL_IP = '';
@@ -99,7 +99,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', 
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', function() {
-  it('test_empty_x_forwarded_for_private_x_real_ip_fallback_on_remote_addr', function() {
+  it('test_empty_http_x_forwarded_for_private_x_real_ip_fallback_on_remote_addr', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '';
     request.headers.HTTP_X_REAL_IP = '192.168.255.182';
@@ -111,7 +111,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', 
 });
 
 describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', function() {
-  it('test_private_x_forward_for_ip_addr', function() {
+  it('test_private_http_x_forward_for_ip_addr', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_FORWARDED_FOR = '127.0.0.1';
     request.headers.HTTP_X_REAL_IP = '';
@@ -147,7 +147,7 @@ describe('get_ip(): IPV4: HTTP_X_FORWARDED_FOR & HTTP_X_REAL_IP & REMOTE_ADDR', 
 });
 
 describe('get_ip(): IPV4: HTTP_X_REAL_IP & REMOTE_ADDR', function() {
-  it('test_missing_x_forwarded', function() {
+  it('test_missing_http_x_forwarded', function() {
     var request = {headers: {}};
     request.headers.HTTP_X_REAL_IP = '177.139.233.132';
     request.headers.REMOTE_ADDR = '177.139.233.133';
@@ -158,7 +158,7 @@ describe('get_ip(): IPV4: HTTP_X_REAL_IP & REMOTE_ADDR', function() {
 });
 
 describe('get_ip(): IPV4: REMOTE_ADDR', function() {
-  it('test_missing_x_forwarded_missing_real_ip', function() {
+  it('test_missing_http_x_forwarded_missing_real_ip', function() {
     var request = {headers: {}};
     request.headers.REMOTE_ADDR = '177.139.233.133';
     get_ip(request);
@@ -189,6 +189,34 @@ describe('get_ip(): IPV4: http_x_real_ip', function() {
     assert.equal(request.clientIpRoutable, true);
   });
 });
+
+
+describe('get_ip(): IPV4: X_FORWARDED_FOR', function() {
+  it('test_x_forwarded_for', function() {
+    var request = {headers: {}};
+    request.headers.HTTP_X_FORWARDED_FOR = '';
+    request.headers.X_FORWARDED_FOR = '177.139.233.132';
+    request.headers.REMOTE_ADDR = '177.139.233.100';
+    get_ip(request);
+    assert.equal(request.clientIp, '177.139.233.132');
+    assert.equal(request.clientIpRoutable, true);
+  });
+});
+
+describe('get_ip(): IPV4: X_FORWARDED_FOR and HTTP_X_FORWARDED_FOR', function() {
+  it('test_http_x_forwarded_for_precedence', function() {
+    var request = {headers: {}};
+    request.headers.HTTP_X_FORWARDED_FOR = '80.10.1.10';
+    request.headers.X_FORWARDED_FOR = '177.139.233.132';
+    request.headers.REMOTE_ADDR = '';
+    get_ip(request);
+    assert.equal(request.clientIp, '80.10.1.10');
+    assert.equal(request.clientIpRoutable, true);
+  });
+});
+
+
+
 
 describe('get_ip(): IPV4: Test 1.0.0.0/8 blocks', function() {
   it('test_1_0_0_0_block', function() {
